6b13eb9f9517fd58b5df80b07c11e12e63c6e4f3
BoldyGo88 <46317043+BoldlyGo88@users.noreply.github.com>
2023-11-01 16:19:21 -0400

    Optimized the splitting function to put any leftover files within Misc
    If the scenes directory contained 7,032 files and was split by 1,350 the user would have 282 files that would be placed within "scenes_6" but now they are placed within Misc removing the need for an entire new directory
    
    In the future I may add Misc partitioning, but I just don't think a mod exists that would require that unless someone was doing the absolute minimum number to split by

--- OTR-Splitter.py
+++ OTR-Splitter.py
@@ -1,6 +1,7 @@
 import json
 import os
 import shutil
+import math
 
 manifest = "manifest.json"
 current_directory = os.getcwd()
@@ -8,8 +9,18 @@ combineable = []
 cthresh = 0
 cmb_manifest = {}
 
-print("How many files will be in each partition, for fat32 and to be safe use a value of 1000 to 1500")
-split_amount = int(input("Enter the max amount of files per split: "))
+print("OTR-Splitter v1.2\n")
+print("1. You can't put a value lower than 1000, doing so will just make it 1000\n2. Default and tested value is 1350\n3. Values <1350 could result in the Misc folder filling and more parts being made todo: fix this one day")
+print("4. Files that are split and converted to .otr with Retro will probably take a lot more space, so plan for that\n")
+print("How many files will be in each partition, for fat32 and to be safe use <=1400")
+split_amount = input("Enter the max amount of files per split: ")
+
+try:
+    split_amount = int(split_amount)
+except ValueError:
+    split_amount = 1350
+    
+if split_amount < 1000: split_amount = 1000
 
 def manifest_path(path):
     return os.path.splitext(path.replace("\\", "/"))[0]
@@ -22,11 +33,21 @@ def ine_create(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
 
+def get_dir_size(directory):
+    count = 0
+    
+    for a, b, files in os.walk(directory):
+        for f in files:
+            count += 1
+    
+    return count
+
 def dir_partition(directory, mdata, combined=False):
     parts = 1
     count = 0
     tmp = {}
     tmp_manifest = {}
+    dir_size = get_dir_size(directory)
     
     if not combined:
         ine_create(f"OTR-Splitter-{directory}_{parts}")
@@ -43,16 +64,25 @@ def dir_partition(directory, mdata, combined=False):
                     tmp_manifest = {}
                     count = 0
                     parts += 1
-            
+                                
                 if count == 0:
                     tmp[f"OTR-Splitter-{directory}_{parts}"] = { "paths": [], "names": []}
-            
-                tmp[f"OTR-Splitter-{directory}_{parts}"]["paths"].append(path)
-                tmp[f"OTR-Splitter-{directory}_{parts}"]["names"].append(os.path.splitext(f)[0])
+                
+                if parts == math.ceil(dir_size/split_amount):
+                    if "OTR-Splitter-Misc" not in tmp:
+                        tmp[f"OTR-Splitter-Misc"] = { "paths": [], "names": []}
+                    tmp["OTR-Splitter-Misc"]["paths"].append(path)
+                    tmp["OTR-Splitter-Misc"]["names"].append(os.path.splitext(f)[0])
+                else:
+                    tmp[f"OTR-Splitter-{directory}_{parts}"]["paths"].append(path)
+                    tmp[f"OTR-Splitter-{directory}_{parts}"]["names"].append(os.path.splitext(f)[0])
              
                 if manifest_path(path) in mdata:
                     print(f"Adding {os.path.splitext(f)[0]} to manifest...")
-                    tmp_manifest[manifest_path(path)] = mdata[manifest_path(path)]
+                    if parts == math.ceil(dir_size/split_amount):
+                        cmb_manifest[manifest_path(path)] = mdata[manifest_path(path)]
+                    else:
+                        tmp_manifest[manifest_path(path)] = mdata[manifest_path(path)]
                     
                 count += 1
     
@@ -89,15 +119,6 @@ def dir_partition(directory, mdata, combined=False):
             print(f"Copying {name}...")
             copy_structure(directory, tmp["OTR-Splitter-Misc"]["paths"][i], "OTR-Splitter-Misc")
             shutil.copy2(f"{tmp['OTR-Splitter-Misc']['paths'][i]}", f"OTR-Splitter-Misc/{tmp['OTR-Splitter-Misc']['paths'][i]}")
-
-def get_dir_size(directory):
-    count = 0
-    
-    for a, b, files in os.walk(directory):
-        for f in files:
-            count += 1
-    
-    return count
 
 if not os.path.exists(manifest):
     raise FileNotFoundError("manifest.json could not be found!")
